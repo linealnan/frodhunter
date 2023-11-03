23214e9a1a5098f0e96ddea769f01da2e8533dc9
Dmitriy Shkatov <DmShkatov@korusconsulting.ru>
2023-11-03 22:01:38 +0300

    [NF] Базовый SDK. Удаление сообщения в канале

--- src/main/java/com/github/linealnan/frodhunter/FrodHunterApplication.java
+++ src/main/java/com/github/linealnan/frodhunter/FrodHunterApplication.java
@@ -1,11 +1,12 @@
 package com.github.linealnan.frodhunter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrodHunterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FrodHunterApplication.class, args);
 	}
--- src/main/java/com/github/linealnan/frodhunter/TelegramLongPollingObserver.java
+++ src/main/java/com/github/linealnan/frodhunter/TelegramLongPollingObserver.java
@@ -1,15 +1,21 @@
 package com.github.linealnan.frodhunter;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
 
 @Component
 public class TelegramLongPollingObserver extends TelegramLongPollingBot {
+    static final Logger log = LoggerFactory.getLogger(FrodHunterApplication.class);
 
     @Getter
     @Value("${bot.name}")
@@ -21,8 +27,10 @@ public class TelegramLongPollingObserver extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         try {
-            sendMessage(update.getMessage().getChatId(), "Привет!");
-        } catch (TelegramApiException e) {
+            Long chatId = update.getChannelPost().getChatId();
+            Integer messageId = update.getChannelPost().getMessageId();
+            removeMessage(chatId, messageId);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -33,6 +41,28 @@ public class TelegramLongPollingObserver extends TelegramLongPollingBot {
         sendMessage.setText(textToSend);
         try {
             execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Use this method to delete a message, including service messages, with the following limitations:
+     * - A message can only be deleted if it was sent less than 48 hours ago.
+     * - Service messages about a supergroup, channel, or forum topic creation can't be deleted.
+     * - A dice message in a private chat can only be deleted if it was sent more than 24 hours ago.
+     * - Bots can delete outgoing messages in private chats, groups, and supergroups.
+     * - Bots can delete incoming messages in private chats.
+     * - Bots granted can_post_messages permissions can delete outgoing messages in channels.
+     * - If the bot is an administrator of a group, it can delete any message there.
+     * - If the bot has can_delete_messages permission in a supergroup or a channel, it can delete any message there.
+     *
+     * @param chatId
+     * @param messageId
+     */
+    private void removeMessage(Long chatId, Integer messageId) throws TelegramApiException {
+        try {
+            execute(new DeleteMessage(chatId.toString(), messageId));
         } catch (TelegramApiException e) {
             throw e;
         }
